e3b754a4612176a8bd78661b44207b7977e89df9
Jh4stYn <jpayehuancar@unsa.edu.pe>
2024-07-29 16:47:43 -0500

    Agregando la ruta a index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const userRoute = require('./user.router')
 
-// colocar las rutas aquí
+router.use('/user', userRoute)
 
 
 module.exports = router;
